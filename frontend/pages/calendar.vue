8a4e479bf8a448d1c67da33166c3a8d6d6eed0f2
AlearvickOne <Alexandr73.73.73.73@mail.ru>
2025-05-14 18:17:35 +0300

    start

---
+++ frontend/pages/calendar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    calendar
+
+
+  </div>
+</template>
---
+++ frontend/pages/login.vue
@@ -0,0 +1,19 @@
+<script>
+import "@/pages/styles/login.scss";
+
+export default {};
+</script>
+
+<template>
+  <div class="wrapper-parent-element login-form">
+    <h1 class="login-title-form">Авторизация</h1>
+
+    <div class="login-inputs-form">
+      <input value="" placeholder="Введите логин" />
+      <input value="" placeholder="Введите пароль" />
+    </div>
+
+    <button class="login-button-form">Войти</button>
+    <button class="login-button-form">Регистрация</button>
+  </div>
+</template>
